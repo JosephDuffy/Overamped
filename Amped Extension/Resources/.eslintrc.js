9e4cdfd6486198b8d02f0ae189c153aea7439bb4
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-24 12:53:54 +0100

    Use TypeScript for content.js

---
+++ Amped Extension/Resources/.eslintrc.js
@@ -0,0 +1,22 @@
+/** @type {import('@types/eslint').Linter.Config} */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "require-await": "error",
+    "no-return-await": "warn",
+  },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+};
