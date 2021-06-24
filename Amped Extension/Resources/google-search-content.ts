dcd5f24e0faafdcea11f9825b9a3b99fc0df7aad
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-24 14:06:03 +0100

    Rename content script, remove AMP icon in results

---
+++ Amped Extension/Resources/google-search-content.ts
@@ -0,0 +1,28 @@
+console.log("Searching for AMP anchors");
+
+document.body.querySelectorAll("a[data-amp-cur]").forEach((element) => {
+  const anchor = element as HTMLAnchorElement;
+
+  const finalURL = anchor.dataset.ampCur!;
+
+  anchor.href = finalURL;
+
+  console.debug("Adding onclick handler to AMP anchor", anchor);
+
+  anchor.onclick = (event) => {
+    event.stopImmediatePropagation();
+
+    if (window.location.pathname.startsWith("/amp/s/")) {
+      console.log("Replacing loaded AMP URL");
+      window.location.replace(finalURL);
+    } else {
+      console.log("Pushing non-AMP URL");
+      window.location.assign(finalURL);
+    }
+
+    return false;
+  };
+
+    const ampIcon = anchor.querySelector("span[aria-label='AMP logo']")
+    ampIcon?.remove()
+});
