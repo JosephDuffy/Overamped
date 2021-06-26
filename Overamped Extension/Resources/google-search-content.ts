99545607b26fb5f4c672dd83ca692d096c126823
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-26 19:42:26 +0100

    Format with updated Prettier rules

--- Overamped Extension/Resources/google-search-content.ts
+++ Overamped Extension/Resources/google-search-content.ts
@@ -1,68 +1,68 @@
 function replaceAMPLinks() {
-  console.debug("Searching for AMP anchors");
+  console.debug("Searching for AMP anchors")
 
   document.body.querySelectorAll("a[data-amp-cur]").forEach((element) => {
-    const anchor = element as HTMLAnchorElement;
+    const anchor = element as HTMLAnchorElement
 
     const finalURL = new URL(
       (() => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const ampCur = anchor.dataset.ampCur!;
+        const ampCur = anchor.dataset.ampCur!
 
         if (ampCur.length > 0) {
-          return ampCur;
+          return ampCur
         }
 
-        return anchor.dataset.cur ?? anchor.href;
-      })()
-    );
+        return anchor.dataset.cur ?? anchor.href
+      })(),
+    )
 
-    console.debug(`URL from attribute: ${finalURL.toString()}`);
+    console.debug(`URL from attribute: ${finalURL.toString()}`)
 
-    const finalSearchParams = new URLSearchParams();
+    const finalSearchParams = new URLSearchParams()
 
     finalURL.searchParams.forEach((value, key) => {
       if (value != "amp" && key != "amp") {
-        console.debug(`Removing ${key}=${value} from final URL`);
-        finalSearchParams.append(key, value);
+        console.debug(`Removing ${key}=${value} from final URL`)
+        finalSearchParams.append(key, value)
       }
-    });
+    })
 
-    finalURL.search = finalSearchParams.toString();
+    finalURL.search = finalSearchParams.toString()
 
     if (finalURL.pathname.startsWith("/amp/")) {
-      console.debug("Removing amp/ prefix");
-      finalURL.pathname = finalURL.pathname.substring(4);
+      console.debug("Removing amp/ prefix")
+      finalURL.pathname = finalURL.pathname.substring(4)
     } else if (finalURL.pathname.endsWith("/amp/")) {
-      console.debug("Removing amp/ postfix");
+      console.debug("Removing amp/ postfix")
       finalURL.pathname = finalURL.pathname.substring(
-        finalURL.pathname.length - "amp/".length
-      );
+        finalURL.pathname.length - "amp/".length,
+      )
     }
 
-    const finalURLString = finalURL.toString();
+    const finalURLString = finalURL.toString()
 
-    console.debug(`De-AMPed URL: ${finalURLString}`);
+    console.debug(`De-AMPed URL: ${finalURLString}`)
 
-    anchor.href = finalURLString;
+    anchor.href = finalURLString
 
-    console.debug("Adding onclick handler to AMP anchor", anchor);
+    console.debug("Adding onclick handler to AMP anchor", anchor)
 
     anchor.onclick = (event) => {
-      event.stopImmediatePropagation();
+      event.stopImmediatePropagation()
 
-      console.debug("Pushing non-AMP URL");
-      window.location.assign(finalURLString);
+      console.debug("Pushing non-AMP URL")
+      window.location.assign(finalURLString)
 
-      return false;
-    };
+      return false
+    }
 
-    const ampIcon = anchor.querySelector("span[aria-label='AMP logo']");
-    ampIcon?.remove();
-  });
+    const ampIcon = anchor.querySelector("span[aria-label='AMP logo']")
+    ampIcon?.remove()
+  })
 }
 
-replaceAMPLinks();
+replaceAMPLinks()
 
 // Support "More Results"
-document.addEventListener("DOMNodeInserted", replaceAMPLinks, false);
+document.addEventListener("DOMNodeInserted", replaceAMPLinks, false)
--- Overamped Extension/Resources/popup.ts
+++ Overamped Extension/Resources/popup.ts
@@ -1,98 +1,98 @@
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const pageURLElement = document.getElementById("currentPage")!; // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const pageURLElement = document.getElementById("currentPage")! // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const toggleAllowListButton = document.getElementById(
-  "toggleAllowListButton"
-)! as HTMLButtonElement;
+  "toggleAllowListButton",
+)! as HTMLButtonElement
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const currentPageDomainSpan = document.getElementById("currentPageDomain")!;
+const currentPageDomainSpan = document.getElementById("currentPageDomain")!
 
-const settingsPromise = browser.storage.local.get("ignoredHostnames");
-const currentTabPromise = browser.tabs.getCurrent();
+const settingsPromise = browser.storage.local.get("ignoredHostnames")
+const currentTabPromise = browser.tabs.getCurrent()
 
 Promise.all([settingsPromise, currentTabPromise]).then(
   ([settings, currentTab]) => {
-    console.debug("Loaded initial settings", settings);
+    console.debug("Loaded initial settings", settings)
 
-    configurePage(settings, currentTab);
+    configurePage(settings, currentTab)
 
     browser.storage.onChanged.addListener((changes) => {
       if (changes["ignoredHostnames"] && changes["ignoredHostnames"].newValue) {
         console.debug(
           "Ignored hostnames setting changed",
-          changes["ignoredHostnames"]
-        );
-        configurePage(changes["ignoredHostnames"].newValue, currentTab);
+          changes["ignoredHostnames"],
+        )
+        configurePage(changes["ignoredHostnames"].newValue, currentTab)
       }
-    });
-  }
-);
+    })
+  },
+)
 
 function configurePage(
   settings: browser.storage.StorageObject,
-  currentTab: browser.tabs.Tab
+  currentTab: browser.tabs.Tab,
 ) {
   if (!currentTab.url) {
-    pageURLElement.innerText = "Failed to load URL";
-    return;
+    pageURLElement.innerText = "Failed to load URL"
+    return
   }
 
-  const ignoredHostnames = settings["ignoredHostnames"] as string[] | undefined;
-  const currentURL = new URL(currentTab.url);
-  currentPageDomainSpan.innerText = currentURL.hostname;
+  const ignoredHostnames = settings["ignoredHostnames"] as string[] | undefined
+  const currentURL = new URL(currentTab.url)
+  currentPageDomainSpan.innerText = currentURL.hostname
 
   if (ignoredHostnames?.includes(currentURL.hostname)) {
-    toggleAllowListButton.innerText = `Disable AMP on ${currentURL.hostname}`;
+    toggleAllowListButton.innerText = `Disable AMP on ${currentURL.hostname}`
 
     toggleAllowListButton.onclick = () => {
-      toggleAllowListButton.disabled = true;
+      toggleAllowListButton.disabled = true
       const newIgnoredHostnames = ignoredHostnames.filter((ignoredHostname) => {
-        ignoredHostname !== currentURL.hostname;
-      });
+        ignoredHostname !== currentURL.hostname
+      })
       browser.storage.local
         .set({
           ignoredHostnames: newIgnoredHostnames,
         })
         .then(() => {
           console.info(
-            `${currentURL.hostname} has been removed from ignore list`
-          );
+            `${currentURL.hostname} has been removed from ignore list`,
+          )
         })
         .catch((error) => {
-          console.error("Failed to save settings", error);
+          console.error("Failed to save settings", error)
         })
         .finally(() => {
-          toggleAllowListButton.disabled = false;
-        });
-      return false;
-    };
+          toggleAllowListButton.disabled = false
+        })
+      return false
+    }
   } else {
-    toggleAllowListButton.innerText = `Enable AMP on ${currentURL.hostname}`;
+    toggleAllowListButton.innerText = `Enable AMP on ${currentURL.hostname}`
 
     toggleAllowListButton.onclick = () => {
-      toggleAllowListButton.disabled = true;
+      toggleAllowListButton.disabled = true
       const newIgnoredHostnames = {
         ignoredHostnames: [...(ignoredHostnames ?? []), currentURL.hostname],
-      };
+      }
       browser.storage.local
         .set({
           ignoredHostnames: newIgnoredHostnames,
         })
         .then(() => {
-          console.info(`${currentURL.hostname} has been added to ignore list`);
+          console.info(`${currentURL.hostname} has been added to ignore list`)
         })
         .catch((error) => {
-          console.error("Failed to save settings", error);
+          console.error("Failed to save settings", error)
         })
         .finally(() => {
-          toggleAllowListButton.disabled = false;
-        });
-      return false;
-    };
+          toggleAllowListButton.disabled = false
+        })
+      return false
+    }
   }
 
   if (currentTab.url) {
-    pageURLElement.innerText = currentTab.url;
+    pageURLElement.innerText = currentTab.url
   } else {
-    pageURLElement.innerText = "Failed to load URL";
+    pageURLElement.innerText = "Failed to load URL"
   }
 }
