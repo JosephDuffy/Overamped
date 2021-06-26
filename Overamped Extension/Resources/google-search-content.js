6fe22574c2021eaa5ba2453dee170d6383b24a78
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-26 13:24:49 +0100

    Support "More Results"

--- Overamped Extension/Resources/google-search-content.js
+++ Overamped Extension/Resources/google-search-content.js
@@ -1,33 +1,37 @@
-console.log("Searching for AMP anchors");
-document.body.querySelectorAll("a[data-amp-cur]").forEach((element) => {
-  const anchor = element;
-  const finalURL = (() => {
-    const ampCur = anchor.dataset.ampCur;
-    if (ampCur.length > 0) {
-      return ampCur;
-    }
-    console.debug("ampCur was empty; possibly a news link");
-    const ampURL = anchor.dataset.cur ?? anchor.href;
-    console.info("AMP URL", ampURL);
-    if (ampURL.endsWith("/amp/")) {
-      return ampURL.substring(0, ampURL.length - "amp/".length);
-    } else {
-      return ampURL;
-    }
-  })();
-  anchor.href = finalURL;
-  console.debug("Adding onclick handler to AMP anchor", anchor);
-  anchor.onclick = (event) => {
-    event.stopImmediatePropagation();
-    if (window.location.pathname.startsWith("/amp/s/")) {
-      console.log("Replacing loaded AMP URL");
-      window.location.replace(finalURL);
-    } else {
-      console.log("Pushing non-AMP URL");
-      window.location.assign(finalURL);
-    }
-    return false;
-  };
-  const ampIcon = anchor.querySelector("span[aria-label='AMP logo']");
-  ampIcon?.remove();
-});
+function replaceAMPLinks() {
+  console.log("Searching for AMP anchors");
+  document.body.querySelectorAll("a[data-amp-cur]").forEach((element) => {
+    const anchor = element;
+    const finalURL = (() => {
+      const ampCur = anchor.dataset.ampCur;
+      if (ampCur.length > 0) {
+        return ampCur;
+      }
+      console.debug("ampCur was empty; possibly a news link");
+      const ampURL = anchor.dataset.cur ?? anchor.href;
+      console.info("AMP URL", ampURL);
+      if (ampURL.endsWith("/amp/")) {
+        return ampURL.substring(0, ampURL.length - "amp/".length);
+      } else {
+        return ampURL;
+      }
+    })();
+    anchor.href = finalURL;
+    console.debug("Adding onclick handler to AMP anchor", anchor);
+    anchor.onclick = (event) => {
+      event.stopImmediatePropagation();
+      if (window.location.pathname.startsWith("/amp/s/")) {
+        console.log("Replacing loaded AMP URL");
+        window.location.replace(finalURL);
+      } else {
+        console.log("Pushing non-AMP URL");
+        window.location.assign(finalURL);
+      }
+      return false;
+    };
+    const ampIcon = anchor.querySelector("span[aria-label='AMP logo']");
+    ampIcon?.remove();
+  });
+}
+replaceAMPLinks();
+document.addEventListener("DOMNodeInserted", replaceAMPLinks, false);
--- Overamped Extension/Resources/google-search-content.ts
+++ Overamped Extension/Resources/google-search-content.ts
@@ -1,48 +1,55 @@
-console.log("Searching for AMP anchors");
+function replaceAMPLinks() {
+  console.log("Searching for AMP anchors");
 
-document.body.querySelectorAll("a[data-amp-cur]").forEach((element) => {
-  const anchor = element as HTMLAnchorElement;
+  document.body.querySelectorAll("a[data-amp-cur]").forEach((element) => {
+    const anchor = element as HTMLAnchorElement;
 
-  const finalURL = (() => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const ampCur = anchor.dataset.ampCur!;
+    const finalURL = (() => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const ampCur = anchor.dataset.ampCur!;
 
-    if (ampCur.length > 0) {
-      return ampCur;
-    }
+      if (ampCur.length > 0) {
+        return ampCur;
+      }
 
-    console.debug("ampCur was empty; possibly a news link");
+      console.debug("ampCur was empty; possibly a news link");
 
-    const ampURL = anchor.dataset.cur ?? anchor.href;
+      const ampURL = anchor.dataset.cur ?? anchor.href;
 
-    console.info("AMP URL", ampURL);
+      console.info("AMP URL", ampURL);
 
-    if (ampURL.endsWith("/amp/")) {
-      // These are often news links.
-      return ampURL.substring(0, ampURL.length - "amp/".length);
-    } else {
-      return ampURL;
-    }
-  })();
+      if (ampURL.endsWith("/amp/")) {
+        // These are often news links.
+        return ampURL.substring(0, ampURL.length - "amp/".length);
+      } else {
+        return ampURL;
+      }
+    })();
 
-  anchor.href = finalURL;
+    anchor.href = finalURL;
 
-  console.debug("Adding onclick handler to AMP anchor", anchor);
+    console.debug("Adding onclick handler to AMP anchor", anchor);
 
-  anchor.onclick = (event) => {
-    event.stopImmediatePropagation();
+    anchor.onclick = (event) => {
+      event.stopImmediatePropagation();
 
-    if (window.location.pathname.startsWith("/amp/s/")) {
-      console.log("Replacing loaded AMP URL");
-      window.location.replace(finalURL);
-    } else {
-      console.log("Pushing non-AMP URL");
-      window.location.assign(finalURL);
-    }
+      if (window.location.pathname.startsWith("/amp/s/")) {
+        console.log("Replacing loaded AMP URL");
+        window.location.replace(finalURL);
+      } else {
+        console.log("Pushing non-AMP URL");
+        window.location.assign(finalURL);
+      }
 
-    return false;
-  };
+      return false;
+    };
 
-  const ampIcon = anchor.querySelector("span[aria-label='AMP logo']");
-  ampIcon?.remove();
-});
+    const ampIcon = anchor.querySelector("span[aria-label='AMP logo']");
+    ampIcon?.remove();
+  });
+}
+
+replaceAMPLinks();
+
+// Support "More Results"
+document.addEventListener("DOMNodeInserted", replaceAMPLinks, false);
