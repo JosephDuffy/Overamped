f2d2f51572279174b3689932a51dda50df52c514
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-27 12:02:18 +0100

    Add instructions to enable extension

---
+++ Overamped/EnableExtensionViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import WebKit
+
+final class EnableExtensionViewController: UIViewController {
+    @IBOutlet private var extensionsRow: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        extensionsRow.layer.cornerRadius = 16
+    }
+}
