74ef22c8f43990557b3ba1f3cd00cc6f0094af4b
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-09 20:56:39 -0700

    Add Locale+googleBaseURL

---
+++ Overamped/Locale+googleBaseURL.swift
@@ -0,0 +1,386 @@
+import Foundation
+
+extension Locale {
+    public var googleBaseURL: URL {
+        switch regionCode {
+        case "AD":
+            return URL(string: "https://www.google.ad")!
+        case "AE":
+            return URL(string: "https://www.google.ae")!
+        case "AF":
+            return URL(string: "https://www.google.com.af")!
+        case "AG":
+            return URL(string: "https://www.google.com.ag")!
+        case "AI":
+            return URL(string: "https://www.google.com.ai")!
+        case "AL":
+            return URL(string: "https://www.google.al")!
+        case "AM":
+            return URL(string: "https://www.google.am")!
+        case "AO":
+            return URL(string: "https://www.google.co.ao")!
+        case "AR":
+            return URL(string: "https://www.google.com.ar")!
+        case "AS":
+            return URL(string: "https://www.google.as")!
+        case "AT":
+            return URL(string: "https://www.google.at")!
+        case "AU":
+            return URL(string: "https://www.google.com.au")!
+        case "AZ":
+            return URL(string: "https://www.google.az")!
+        case "BA":
+            return URL(string: "https://www.google.ba")!
+        case "BD":
+            return URL(string: "https://www.google.com.bd")!
+        case "BE":
+            return URL(string: "https://www.google.be")!
+        case "BF":
+            return URL(string: "https://www.google.bf")!
+        case "BG":
+            return URL(string: "https://www.google.bg")!
+        case "BH":
+            return URL(string: "https://www.google.com.bh")!
+        case "BI":
+            return URL(string: "https://www.google.bi")!
+        case "BJ":
+            return URL(string: "https://www.google.bj")!
+        case "BN":
+            return URL(string: "https://www.google.com.bn")!
+        case "BO":
+            return URL(string: "https://www.google.com.bo")!
+        case "BR":
+            return URL(string: "https://www.google.com.br")!
+        case "BS":
+            return URL(string: "https://www.google.bs")!
+        case "BT":
+            return URL(string: "https://www.google.bt")!
+        case "BW":
+            return URL(string: "https://www.google.co.bw")!
+        case "BY":
+            return URL(string: "https://www.google.by")!
+        case "BZ":
+            return URL(string: "https://www.google.com.bz")!
+        case "CA":
+            return URL(string: "https://www.google.ca")!
+        case "CD":
+            return URL(string: "https://www.google.cd")!
+        case "CF":
+            return URL(string: "https://www.google.cf")!
+        case "CG":
+            return URL(string: "https://www.google.cg")!
+        case "CH":
+            return URL(string: "https://www.google.ch")!
+        case "CI":
+            return URL(string: "https://www.google.ci")!
+        case "CK":
+            return URL(string: "https://www.google.co.ck")!
+        case "CL":
+            return URL(string: "https://www.google.cl")!
+        case "CM":
+            return URL(string: "https://www.google.cm")!
+        case "CN":
+            return URL(string: "https://www.google.cn")!
+        case "CO":
+            return URL(string: "https://www.google.com.co")!
+        case "CR":
+            return URL(string: "https://www.google.co.cr")!
+        case "CU":
+            return URL(string: "https://www.google.com.cu")!
+        case "CV":
+            return URL(string: "https://www.google.cv")!
+        case "CY":
+            return URL(string: "https://www.google.com.cy")!
+        case "CZ":
+            return URL(string: "https://www.google.cz")!
+        case "DE":
+            return URL(string: "https://www.google.de")!
+        case "DJ":
+            return URL(string: "https://www.google.dj")!
+        case "DK":
+            return URL(string: "https://www.google.dk")!
+        case "DM":
+            return URL(string: "https://www.google.dm")!
+        case "DO":
+            return URL(string: "https://www.google.com.do")!
+        case "DZ":
+            return URL(string: "https://www.google.dz")!
+        case "EC":
+            return URL(string: "https://www.google.com.ec")!
+        case "EE":
+            return URL(string: "https://www.google.ee")!
+        case "EG":
+            return URL(string: "https://www.google.com.eg")!
+        case "ES":
+            return URL(string: "https://www.google.es")!
+        case "ET":
+            return URL(string: "https://www.google.com.et")!
+        case "FI":
+            return URL(string: "https://www.google.fi")!
+        case "FJ":
+            return URL(string: "https://www.google.com.fj")!
+        case "FM":
+            return URL(string: "https://www.google.fm")!
+        case "FR":
+            return URL(string: "https://www.google.fr")!
+        case "GA":
+            return URL(string: "https://www.google.ga")!
+        case "GE":
+            return URL(string: "https://www.google.ge")!
+        case "GG":
+            return URL(string: "https://www.google.gg")!
+        case "GH":
+            return URL(string: "https://www.google.com.gh")!
+        case "GI":
+            return URL(string: "https://www.google.com.gi")!
+        case "GL":
+            return URL(string: "https://www.google.gl")!
+        case "GM":
+            return URL(string: "https://www.google.gm")!
+        case "GR":
+            return URL(string: "https://www.google.gr")!
+        case "GT":
+            return URL(string: "https://www.google.com.gt")!
+        case "GY":
+            return URL(string: "https://www.google.gy")!
+        case "HK":
+            return URL(string: "https://www.google.com.hk")!
+        case "HN":
+            return URL(string: "https://www.google.hn")!
+        case "HR":
+            return URL(string: "https://www.google.hr")!
+        case "HT":
+            return URL(string: "https://www.google.ht")!
+        case "HU":
+            return URL(string: "https://www.google.hu")!
+        case "ID":
+            return URL(string: "https://www.google.co.id")!
+        case "IE":
+            return URL(string: "https://www.google.ie")!
+        case "IL":
+            return URL(string: "https://www.google.co.il")!
+        case "IM":
+            return URL(string: "https://www.google.im")!
+        case "IN":
+            return URL(string: "https://www.google.co.in")!
+        case "IQ":
+            return URL(string: "https://www.google.iq")!
+        case "IS":
+            return URL(string: "https://www.google.is")!
+        case "IT":
+            return URL(string: "https://www.google.it")!
+        case "JE":
+            return URL(string: "https://www.google.je")!
+        case "JM":
+            return URL(string: "https://www.google.com.jm")!
+        case "JO":
+            return URL(string: "https://www.google.jo")!
+        case "JP":
+            return URL(string: "https://www.google.co.jp")!
+        case "KE":
+            return URL(string: "https://www.google.co.ke")!
+        case "KH":
+            return URL(string: "https://www.google.com.kh")!
+        case "KI":
+            return URL(string: "https://www.google.ki")!
+        case "KG":
+            return URL(string: "https://www.google.kg")!
+        case "KR":
+            return URL(string: "https://www.google.co.kr")!
+        case "KW":
+            return URL(string: "https://www.google.com.kw")!
+        case "KZ":
+            return URL(string: "https://www.google.kz")!
+        case "LA":
+            return URL(string: "https://www.google.la")!
+        case "LB":
+            return URL(string: "https://www.google.com.lb")!
+        case "LI":
+            return URL(string: "https://www.google.li")!
+        case "LK":
+            return URL(string: "https://www.google.lk")!
+        case "LS":
+            return URL(string: "https://www.google.co.ls")!
+        case "LT":
+            return URL(string: "https://www.google.lt")!
+        case "LU":
+            return URL(string: "https://www.google.lu")!
+        case "LV":
+            return URL(string: "https://www.google.lv")!
+        case "LY":
+            return URL(string: "https://www.google.com.ly")!
+        case "MA":
+            return URL(string: "https://www.google.co.ma")!
+        case "MD":
+            return URL(string: "https://www.google.md")!
+        case "ME":
+            return URL(string: "https://www.google.me")!
+        case "MG":
+            return URL(string: "https://www.google.mg")!
+        case "MK":
+            return URL(string: "https://www.google.mk")!
+        case "ML":
+            return URL(string: "https://www.google.ml")!
+        case "MM":
+            return URL(string: "https://www.google.com.mm")!
+        case "MN":
+            return URL(string: "https://www.google.mn")!
+        case "MS":
+            return URL(string: "https://www.google.ms")!
+        case "MT":
+            return URL(string: "https://www.google.com.mt")!
+        case "MU":
+            return URL(string: "https://www.google.mu")!
+        case "MV":
+            return URL(string: "https://www.google.mv")!
+        case "MW":
+            return URL(string: "https://www.google.mw")!
+        case "MX":
+            return URL(string: "https://www.google.com.mx")!
+        case "MY":
+            return URL(string: "https://www.google.com.my")!
+        case "MZ":
+            return URL(string: "https://www.google.co.mz")!
+        case "NA":
+            return URL(string: "https://www.google.com.na")!
+        case "NG":
+            return URL(string: "https://www.google.com.ng")!
+        case "NI":
+            return URL(string: "https://www.google.com.ni")!
+        case "NE":
+            return URL(string: "https://www.google.ne")!
+        case "NL":
+            return URL(string: "https://www.google.nl")!
+        case "NO":
+            return URL(string: "https://www.google.no")!
+        case "NP":
+            return URL(string: "https://www.google.com.np")!
+        case "NR":
+            return URL(string: "https://www.google.nr")!
+        case "NU":
+            return URL(string: "https://www.google.nu")!
+        case "NZ":
+            return URL(string: "https://www.google.co.nz")!
+        case "OM":
+            return URL(string: "https://www.google.com.om")!
+        case "PA":
+            return URL(string: "https://www.google.com.pa")!
+        case "PE":
+            return URL(string: "https://www.google.com.pe")!
+        case "PG":
+            return URL(string: "https://www.google.com.pg")!
+        case "PH":
+            return URL(string: "https://www.google.com.ph")!
+        case "PK":
+            return URL(string: "https://www.google.com.pk")!
+        case "PL":
+            return URL(string: "https://www.google.pl")!
+        case "PN":
+            return URL(string: "https://www.google.pn")!
+        case "PR":
+            return URL(string: "https://www.google.com.pr")!
+        case "PS":
+            return URL(string: "https://www.google.ps")!
+        case "PT":
+            return URL(string: "https://www.google.pt")!
+        case "PY":
+            return URL(string: "https://www.google.com.py")!
+        case "QA":
+            return URL(string: "https://www.google.com.qa")!
+        case "RO":
+            return URL(string: "https://www.google.ro")!
+        case "RU":
+            return URL(string: "https://www.google.ru")!
+        case "RW":
+            return URL(string: "https://www.google.rw")!
+        case "SA":
+            return URL(string: "https://www.google.com.sa")!
+        case "SB":
+            return URL(string: "https://www.google.com.sb")!
+        case "SC":
+            return URL(string: "https://www.google.sc")!
+        case "SE":
+            return URL(string: "https://www.google.se")!
+        case "SG":
+            return URL(string: "https://www.google.com.sg")!
+        case "SH":
+            return URL(string: "https://www.google.sh")!
+        case "SI":
+            return URL(string: "https://www.google.si")!
+        case "SK":
+            return URL(string: "https://www.google.sk")!
+        case "SL":
+            return URL(string: "https://www.google.com.sl")!
+        case "SN":
+            return URL(string: "https://www.google.sn")!
+        case "SO":
+            return URL(string: "https://www.google.so")!
+        case "SM":
+            return URL(string: "https://www.google.sm")!
+        case "SR":
+            return URL(string: "https://www.google.sr")!
+        case "ST":
+            return URL(string: "https://www.google.st")!
+        case "SV":
+            return URL(string: "https://www.google.com.sv")!
+        case "TD":
+            return URL(string: "https://www.google.td")!
+        case "TG":
+            return URL(string: "https://www.google.tg")!
+        case "TH":
+            return URL(string: "https://www.google.co.th")!
+        case "TJ":
+            return URL(string: "https://www.google.com.tj")!
+        case "TL":
+            return URL(string: "https://www.google.tl")!
+        case "TM":
+            return URL(string: "https://www.google.tm")!
+        case "TN":
+            return URL(string: "https://www.google.tn")!
+        case "TO":
+            return URL(string: "https://www.google.to")!
+        case "TR":
+            return URL(string: "https://www.google.com.tr")!
+        case "TT":
+            return URL(string: "https://www.google.tt")!
+        case "TW":
+            return URL(string: "https://www.google.com.tw")!
+        case "TZ":
+            return URL(string: "https://www.google.co.tz")!
+        case "UA":
+            return URL(string: "https://www.google.com.ua")!
+        case "UG":
+            return URL(string: "https://www.google.co.ug")!
+        case "GB":
+            return URL(string: "https://www.google.co.uk")!
+        case "UY":
+            return URL(string: "https://www.google.com.uy")!
+        case "UZ":
+            return URL(string: "https://www.google.co.uz")!
+        case "VC":
+            return URL(string: "https://www.google.com.vc")!
+        case "VE":
+            return URL(string: "https://www.google.co.ve")!
+        case "VG":
+            return URL(string: "https://www.google.vg")!
+        case "VI":
+            return URL(string: "https://www.google.co.vi")!
+        case "VN":
+            return URL(string: "https://www.google.com.vn")!
+        case "VU":
+            return URL(string: "https://www.google.vu")!
+        case "WS":
+            return URL(string: "https://www.google.ws")!
+        case "RS":
+            return URL(string: "https://www.google.rs")!
+        case "ZA":
+            return URL(string: "https://www.google.co.za")!
+        case "ZM":
+            return URL(string: "https://www.google.co.zm")!
+        case "ZW":
+            return URL(string: "https://www.google.co.zw")!
+        default:
+            return URL(string: "https://www.google.com")!
+        }
+    }
+}
