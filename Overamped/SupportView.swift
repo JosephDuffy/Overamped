5dd32975b5768242b331e712bec121de68431026
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-16 18:48:40 -0700

    Add "Write a Review" section

--- Overamped/SupportView.swift
+++ Overamped/SupportView.swift
@@ -20,6 +20,22 @@ struct SupportView: View {
                 }
                 .buttonStyle(BorderedButtonStyle())
 
+                Text("Write a Review")
+                    .font(.title)
+
+                Text("Reviews are very important on the App Store, plus I love to read them.")
+
+                Link(
+                    destination: URL(string: "https://itunes.apple.com/app/id1573901090?action=write-review&mt=8")!,
+                    label: {
+                        Text("\(Image(systemName: "square.and.pencil")) Review Overamped")
+                    }
+                )
+                    .buttonStyle(BorderedButtonStyle())
+
+                Text("Having an issue? [Submit feedback instead](overamped:feedback).")
+                    .font(.caption)
+
                 TipJarView()
             }
             .padding()
