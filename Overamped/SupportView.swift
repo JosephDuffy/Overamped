aaa78f9db542f6b6b28568fb97f45daa5510a68f
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-16 18:48:08 -0700

    Reduce spacing from 16 to 12

--- Overamped/SupportView.swift
+++ Overamped/SupportView.swift
@@ -5,7 +5,7 @@ struct SupportView: View {
 
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 16) {
+            VStack(alignment: .leading, spacing: 12) {
                 Text("""
                     Overamped is developed by a single indie developer. If you're able to provide any extra support it is greatly appreciated.
                     """)
