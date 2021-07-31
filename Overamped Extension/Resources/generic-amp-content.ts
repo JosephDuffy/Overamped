83f5defc9fb5322bd32301a4512c0c1fe5d1f9f9
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-30 17:50:23 -0700

    Add script to redirect any AMP content

---
+++ Overamped Extension/Resources/generic-amp-content.ts
@@ -0,0 +1,38 @@
+import ExtensionApplicator from "./ExtensionApplicator"
+import NativeAppCommunicator from "./NativeAppCommunicator"
+
+function redirectToCanonicalVersion(ignoredHostnames: string[]) {
+  const documentAttributes = document.documentElement.attributes
+
+  if (
+    !Object.prototype.hasOwnProperty.call(documentAttributes, "amp") &&
+    !Object.prototype.hasOwnProperty.call(documentAttributes, "⚡")
+  ) {
+    return
+  }
+
+  const canonicalElement: HTMLLinkElement | null = document.head.querySelector(
+    "link[rel~='canonical'][href]",
+  )
+
+  if (!canonicalElement) {
+    console.debug("Couldn't find canonical URL to redirect to")
+    return
+  }
+
+  const canonicalURL = new URL(canonicalElement.href)
+
+  if (ignoredHostnames.includes(canonicalURL.hostname)) {
+    console.info(
+      `Not redirecting because ${canonicalURL.hostname} is in the ignored hostnames`,
+    )
+  } else {
+    console.log(`Redirecting AMP page to ${canonicalURL.toString()}`)
+
+    new NativeAppCommunicator().logRedirectedLink(canonicalURL)
+
+    window.location.replace(canonicalURL.toString())
+  }
+}
+
+new ExtensionApplicator(document, redirectToCanonicalVersion, false)
