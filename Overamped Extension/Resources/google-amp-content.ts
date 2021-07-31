9a262a7e22c6ca067c546ff28a086cf37dbbe5fa
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-30 17:10:39 -0700

    Use original URL when redirect Google's AMP cache

--- Overamped Extension/Resources/google-amp-content.ts
+++ Overamped Extension/Resources/google-amp-content.ts
@@ -1,27 +1,50 @@
-import deampURL from "./deampURL"
 import ExtensionApplicator from "./ExtensionApplicator"
+import NativeAppCommunicator from "./NativeAppCommunicator"
 
 function redirectToCanonicalVersion(ignoredHostnames: string[]) {
-  const canonicalElement: HTMLLinkElement | null = document.head.querySelector(
-    "link[rel~='canonical'][href]",
-  )
+  const canonicalAnchor: HTMLAnchorElement | null =
+    document.querySelector("a.amp-canurl")
 
-  if (!canonicalElement) {
-    console.debug("Couldn't find canonical URL to redirect to")
-    return
-  }
+  if (canonicalAnchor) {
+    // This is the actual URL of the non-AMP page
+    const canonicalURL = new URL(canonicalAnchor.href)
 
-  const canonicalURL = new URL(canonicalElement.href)
-  const finalURL = deampURL(canonicalURL)
+    if (ignoredHostnames.includes(canonicalURL.hostname)) {
+      console.info(
+        `Not redirecting because ${canonicalURL.hostname} is in the ignored hostnames`,
+      )
+    } else {
+      console.log(`Redirecting AMP page to ${canonicalURL.toString()}`)
 
-  if (ignoredHostnames.includes(finalURL.hostname)) {
-    console.info(
-      `Not redirecting because ${finalURL.hostname} is in the ignored hostnames`,
-    )
-  } else {
-    console.log(`Redirecting AMP page to ${finalURL.toString()}`)
+      new NativeAppCommunicator().logRedirectedLink(canonicalURL)
 
-    window.location.replace(finalURL.toString())
+      window.location.replace(canonicalURL.toString())
+    }
+  } else if (document.readyState === "complete") {
+    // Google may have changed something so the canonical link is used as a fallback.
+    // This isn't ideal because the canonical link is the AMP version of the website
+    // hosted by the website, not the actual canonical non-AMP version.
+    const canonicalElement: HTMLLinkElement | null =
+      document.head.querySelector("link[rel~='canonical'][href]")
+
+    if (!canonicalElement) {
+      console.debug("Couldn't find canonical URL to redirect to")
+      return
+    }
+
+    const canonicalURL = new URL(canonicalElement.href)
+
+    if (ignoredHostnames.includes(canonicalURL.hostname)) {
+      console.info(
+        `Not redirecting because ${canonicalURL.hostname} is in the ignored hostnames`,
+      )
+    } else {
+      console.log(`Redirecting AMP page to ${canonicalURL.toString()}`)
+
+      new NativeAppCommunicator().logRedirectedLink(canonicalURL)
+
+      window.location.replace(canonicalURL.toString())
+    }
   }
 }
 
