67d98c92b6cbe46955540047dcb58b6f7e514871
Joseph Duffy <commits@josephduffy.co.uk>
2021-07-31 21:04:06 -0700

    Add ignored hostnames link in Settings tab

--- Overamped/SettingsView.swift
+++ Overamped/SettingsView.swift
@@ -1,13 +1,35 @@
+import OverampedCore
+import Persist
 import SwiftUI
 
 struct SettingsView: View {
     @AppStorage("enabledAdvancedStatistics")
     private var enabledAdvancedStatistics = false
 
+    @SceneStorage("SettingsView.isShowingIgnoredHostnames")
+    private var isShowingIgnoredHostnames = false
+
+    @PersistStorage(persister: .ignoredHostnames)
+    private(set) var ignoredHostnames: [String]
+
     var body: some View {
         List {
             Section(footer: Text("Enable advanced statistics to collect the domains and timestamps of replaced and redirect links.")) {
                 Toggle(isOn: $enabledAdvancedStatistics, label: { Text("Advanced Statistics") })
+
+            Section(footer: Text("Overamped will not redirect to the canonical version of websites it has been disabled on.")) {
+                NavigationLink(
+                    isActive: $isShowingIgnoredHostnames
+                ) {
+                    IgnoredHostnamesView()
+                } label: {
+                    HStack {
+                        Text("Disabled Websites")
+                        Spacer()
+                        Text(ignoredHostnames.count.formatted())
+                            .foregroundColor(Color(.placeholderText))
+                    }
+                }
             }
         }
     }
