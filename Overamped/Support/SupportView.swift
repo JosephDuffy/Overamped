ee55af96c68cc25ac43632d9be3c0c2b25ceea3a
Joseph Duffy <commits@josephduffy.co.uk>
2021-08-21 11:55:43 -0700

    Improve layout of Support view on iPadOS

--- Overamped/Support/SupportView.swift
+++ Overamped/Support/SupportView.swift
@@ -10,75 +10,89 @@ struct SupportView: View {
     @AppStorage("HasSubmittedPricingSurvey")
     private var hasSubmittedPricingSurvey = false
 
+    @Environment(\.horizontalSizeClass)
+    private var horizontalSizeClass
+
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 12) {
+            VStack(alignment: .leading, spacing: horizontalSizeClass == .regular ? 0 : 12) {
                 Text("""
                     Overamped is developed by a single indie developer. If you're able to provide any extra support it is greatly appreciated.
                     """)
+                    .padding(.vertical, horizontalSizeClass == .regular ? 12 : 0)
 
-                Text("✈️ Pricing Survey")
-                    .font(.title)
-
-                if hasSubmittedPricingSurvey {
-                    Text("Thank you for completing the pricing survey.")
-                } else {
-                    Text("A short survey is available for TestFlight users. It is 2 questions and will help decide the pricing model for Overamped.")
-
-                    NavigationLink(isActive: $isShowingSurvey) {
-                        SurveyView()
-                    } label: {
-                        Text("Complete Survey")
+                if horizontalSizeClass == .regular {
+                    Divider()
+                    HStack(alignment: .top) {
+                        shareSection
+                            .padding(.vertical, 12)
+                        Divider()
+                        reviewSection
+                            .padding(.vertical, 12)
                     }
-                    .buttonStyle(BorderedButtonStyle())
+                    Divider()
+                } else {
+                    shareSection
+                    reviewSection
                 }
-
-                Text("Share")
-                    .font(.title)
-
-                Text("The easiest way to support Overamped is to share Overamped with friends and on social media.")
-
-                Button("\(Image(systemName: "square.and.arrow.up")) Share Overamped") {
-                    showShareSheet = true
-                }
-                .buttonStyle(BorderedButtonStyle())
-                .background(
-                    ActivityView(
-                        isPresented: $showShareSheet,
-                        items: {
-                            [
-                                "Download Overamped to disable AMP in Safari",
-                                URLUIActivityItemSource(
-                                    url: URL(string: "https://overamped.app")!
-                                ),
-                            ]
-                        }
-                    )
-                )
 
                 if DistributionMethod.current == .debug {
-                    Text("Write a Review")
-                        .font(.title)
-
-                    Text("Reviews are very important on the App Store. You can also rate Overamped without writing a review.")
-
-                    Link(
-                        destination: URL(string: "https://itunes.apple.com/app/id1573901090?action=write-review&mt=8")!,
-                        label: {
-                            Text("\(Image(systemName: "square.and.pencil")) Review Overamped")
-                        }
-                    )
-                        .buttonStyle(BorderedButtonStyle())
-
-                    Text("Having an issue? [Submit feedback instead](overamped:feedback).")
-                        .font(.caption)
-
                     TipJarView()
+                        .padding(.vertical, horizontalSizeClass == .regular ? 12 : 0)
                 }
             }
             .padding()
         }
         .navigationTitle("Support Overamped")
+    }
+
+    @ViewBuilder
+    private var shareSection: some View {
+        VStack(alignment: horizontalSizeClass == .regular ? .leading : .leading) {
+            Text("Share")
+                .font(.title)
+
+            Text("The easiest way to support Overamped is to share Overamped with friends and on social media.")
+
+            Button("\(Image(systemName: "square.and.arrow.up")) Share Overamped") {
+                showShareSheet = true
+            }
+            .buttonStyle(BorderedButtonStyle())
+            .background(
+                ActivityView(
+                    isPresented: $showShareSheet,
+                    items: {
+                        [
+                            "Download Overamped to disable AMP in Safari",
+                            URLUIActivityItemSource(
+                                url: URL(string: "https://overamped.app")!
+                            ),
+                        ]
+                    }
+                )
+            )
+        }
+    }
+
+    @ViewBuilder
+    private var reviewSection: some View {
+        VStack(alignment: horizontalSizeClass == .regular ? .leading : .leading) {
+            Text("Write a Review")
+                .font(.title)
+
+            Text("Reviews are very important on the App Store. You can also rate Overamped without writing a review.")
+
+            Link(
+                destination: URL(string: "https://itunes.apple.com/app/id1573901090?action=write-review&mt=8")!,
+                label: {
+                    Text("\(Image(systemName: "square.and.pencil")) Review Overamped")
+                }
+            )
+                .buttonStyle(BorderedButtonStyle())
+
+            Text("Having an issue? [Submit feedback instead](overamped:feedback).")
+                .font(.caption)
+        }
     }
 }
 
