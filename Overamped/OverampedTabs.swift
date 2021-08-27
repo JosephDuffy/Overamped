1a740c69dc774d149b214c190f9e222162d1b1b0
Joseph Duffy <commits@josephduffy.co.uk>
2021-08-26 21:54:26 -0700

    Use sidebar in place of tabs on iPad

--- Overamped/OverampedTabs.swift
+++ Overamped/OverampedTabs.swift
@@ -2,93 +2,147 @@ import SwiftUI
 import os.log
 
 struct OverampedTabs: View {
-    enum Tab: String {
+    enum Tab: String, CaseIterable {
         case statistics
-        case feedback
         case support
+        case feedback
         case settings
         case about
+
+        var title: LocalizedStringKey {
+            switch self {
+            case .statistics:
+                return "Statistics"
+            case .support:
+                return "Support"
+            case .feedback:
+                return "Feedback"
+            case .settings:
+                return "Settings"
+            case .about:
+                return "About"
+            }
+        }
+
+        var icon: Image {
+            switch self {
+            case .statistics:
+                return Image(systemName: "chart.xyaxis.line")
+            case .support:
+                return Image(systemName: "heart.fill")
+            case .feedback:
+                return Image(systemName: "envelope")
+            case .settings:
+                return Image(systemName: "gear")
+            case .about:
+                return Image(systemName: "info.circle")
+            }
+        }
+
+        @ViewBuilder
+        var destination: some View {
+            switch self {
+            case .statistics:
+                StatisticsView()
+            case .support:
+                SupportView()
+            case .feedback:
+                FeedbackView()
+            case .settings:
+                SettingsView()
+            case .about:
+                AboutView()
+            }
+        }
     }
 
     @SceneStorage("OverampedTabs.selectedTab")
-    private var selectedTab: Tab = .statistics
+    private var selectedTab: Tab = .support
+
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
 
     var body: some View {
+        if horizontalSizeClass == .compact {
+            tabsView
+        } else {
+            sidebarView
+        }
+
+        EmptyView()
+            .onOpenURL(perform: { url in
+                guard let deepLink = DeepLink(url: url) else { return }
+
+                switch deepLink {
+                case .statistics:
+                    selectedTab = .statistics
+                case .support:
+                    selectedTab = .support
+                case .feedback:
+                    selectedTab = .feedback
+                case .settings:
+                    selectedTab = .settings
+                case .about:
+                    selectedTab = .about
+                case .debug:
+                    break
+                }
+            })
+    }
+
+    private var tabsView: some View {
         TabView(selection: $selectedTab) {
-            NavigationView {
-                StatisticsView()
-            }
-            .tag(Tab.statistics)
-            .tabItem {
-                VStack {
-                    Image(systemName: "chart.xyaxis.line")
-                    Text("Statistics")
+            ForEach(OverampedTabs.Tab.allCases, id: \.self) { tab in
+                NavigationView {
+                    tab.destination
                 }
-            }
-
-            NavigationView {
-                SupportView()
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
-            .tag(Tab.support)
-            .tabItem {
-                VStack {
-                    Image(systemName: "heart.fill")
-                    Text("Support")
-                }
-            }
-
-            NavigationView {
-                FeedbackForm()
-            }
-            .navigationViewStyle(StackNavigationViewStyle())
-            .tag(Tab.feedback)
-            .tabItem {
-                VStack {
-                    Image(systemName: "envelope")
-                    Text("Feedback")
-                }
-            }
-
-            NavigationView {
-                SettingsView()
-            }
-            .tag(Tab.settings)
-            .tabItem {
-                VStack {
-                    Image(systemName: "gear")
-                    Text("Settings")
-                }
-            }
-
-            NavigationView {
-                AboutView()
-            }
-            .tag(Tab.about)
-            .tabItem {
-                VStack {
-                    Image(systemName: "info.circle")
-                    Text("About")
+                .tag(tab)
+                .tabItem {
+                    VStack {
+                        tab.icon
+                        Text(tab.title)
+                    }
                 }
             }
         }
-        .onOpenURL(perform: { url in
-            guard let deepLink = DeepLink(url: url) else { return }
+    }
 
-            switch deepLink {
-            case .statistics:
-                selectedTab = .statistics
-            case .support:
-                selectedTab = .support
-            case .feedback:
-                selectedTab = .feedback
-            case .settings:
-                selectedTab = .settings
-            case .about:
-                selectedTab = .about
-            case .debug:
-                break
+    private var sidebarView: some View {
+        NavigationView {
+            Sidebar(selectedTab: $selectedTab)
+            selectedTab.destination
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct Sidebar: View {
+    @Binding var selectedTab: OverampedTabs.Tab?
+
+    @ViewBuilder
+    var body: some View {
+        List {
+            ForEach(OverampedTabs.Tab.allCases, id: \.self) { tab in
+                Button {
+                    selectedTab = tab
+                } label: {
+                    Label {
+                        Text(tab.title)
+                    } icon: {
+                        tab.icon
+                    }
+                }
+                .listRowBackground(
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundColor(tab == selectedTab ? Color(.systemFill) : .clear)
+                )
+                .tag(tab)
             }
-        })
+        }
+        .navigationTitle("Overamped")
+        .listStyle(SidebarListStyle())
+    }
+
+    init(selectedTab: Binding<OverampedTabs.Tab>) {
+        _selectedTab = Binding(get: { selectedTab.wrappedValue }, set: { selectedTab.wrappedValue = $0 ?? .statistics })
     }
 }
