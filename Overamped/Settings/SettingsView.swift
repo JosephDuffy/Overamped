8bea6eb4921e6651d28b8a8dcfde6a3ec4c1c6bb
Joseph Duffy <commits@josephduffy.co.uk>
2021-09-16 00:23:51 +0100

    Always show clear statistics button, disable when statistics are empty

--- Overamped/Settings/SettingsView.swift
+++ Overamped/Settings/SettingsView.swift
@@ -9,8 +9,21 @@ struct SettingsView: View {
     @SceneStorage("SettingsView.isShowingIgnoredHostnames")
     private var isShowingIgnoredHostnames = false
 
+    // MARK: Basic statistics
+
     @PersistStorage(persister: .basicStatisticsResetDate)
     private var basicStatisticsResetDate: Date?
+
+    @PersistStorage(persister: .replacedLinksCount)
+    private var replacedLinksCount: Int
+
+    @PersistStorage(persister: .redirectedLinksCount)
+    private var redirectedLinksCount: Int
+
+    @State
+    private var hasCollectedAnyBasicStatistics = false
+
+    // MARK: Advanced statistics
 
     @PersistStorage(persister: .advancedStatisticsResetDate)
     private var advancedStatisticsResetDate: Date?
@@ -24,6 +37,8 @@ struct SettingsView: View {
     @State
     private var hasCollectedAnyAdvancesStatistics = false
 
+    // MARK: Ignored hostnames
+
     @PersistStorage(persister: .ignoredHostnames)
     private(set) var ignoredHostnames: [String]
 
@@ -31,6 +46,10 @@ struct SettingsView: View {
         List {
             Section(footer: Text("Basic statistics last reset: \(basicStatisticsResetDate?.formatted() ?? "Never").")) {
                 ClearBasicStatisticsView()
+                    .onReceive(_replacedLinksCount.persister.publisher.combineLatest(_redirectedLinksCount.persister.publisher)) { (replacedLinksCount, redirectedLinksCount) in
+                        hasCollectedAnyBasicStatistics = replacedLinksCount > 0 || redirectedLinksCount > 0
+                    }
+                    .disabled(!hasCollectedAnyBasicStatistics)
             }
 
             Section(footer: Text("Advanced statistics includes the domains and timestamps of replaced and redirect links.\nAdvanced statistics last reset: \(advancedStatisticsResetDate?.formatted() ?? "Never").")) {
@@ -50,9 +69,8 @@ struct SettingsView: View {
                         hasCollectedAnyAdvancesStatistics = false
                     }
 
-                if hasCollectedAnyAdvancesStatistics {
-                    ClearAdvancedStatisticsView()
-                }
+                ClearAdvancedStatisticsView()
+                    .disabled(!hasCollectedAnyAdvancesStatistics)
             }
 
             Section(footer: Text("Overamped will not redirect to the canonical version of websites it has been disabled on.")) {
