5689a462153ddc8c75bf72d2e20adb25267aa759
Joseph Duffy <commits@josephduffy.co.uk>
2021-10-17 19:18:22 +0100

    Use swiftui-support branch of Persist

--- OverampedCore/Package.swift
+++ OverampedCore/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "OverampedCore", targets: ["OverampedCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/JosephDuffy/Persist.git", .branch("swiftui-support-xcode-13-beta3")),
+        .package(url: "https://github.com/JosephDuffy/Persist.git", .branch("swiftui-support")),
     ],
     targets: [
         .target(name: "OverampedCore", dependencies: ["Persist"]),
