f8f948c136d440c59dbc9519fa0edffa2f2c3907
Joseph Duffy <commits@josephduffy.co.uk>
2022-01-28 23:39:10 -0800

    Improve initial performance of events log, add `EventsLog`

--- Overamped/Statistics/EventsLogView.swift
+++ Overamped/Statistics/EventsLogView.swift
@@ -4,33 +4,36 @@ import SwiftUI
 import OverampedCore
 
 struct EventsLogView: View {
-    @PersistStorage(persister: .replacedLinks)
-    private var replacedLinks: [ReplacedLinksEvent]
-
-    @PersistStorage(persister: .redirectedLinks)
-    private var redirectedLinks: [RedirectLinkEvent]
-
-    @State private var searchText: String = ""
-
-    private var events: [Event] {
-        let allEvents = replacedLinks.map { Event.replacedLinks($0) } + redirectedLinks.map { Event.redirectedLink($0) }
-        var events = allEvents.sorted(by: { $0.date > $1.date })
-
-        if !searchText.isEmpty {
-            events = events.filter { event -> Bool in
-                switch event {
-                case .replacedLinks(let event):
-                    return event.domains.contains { $0.localizedCaseInsensitiveContains(searchText) }
-                case .redirectedLink(let event):
-                    return event.domain.localizedCaseInsensitiveContains(searchText)
-                }
-            }
-        }
-
-        return events
-    }
+    @StateObject private var eventsLog = EventsLog()
 
     var body: some View {
+        VStack {
+            if let events = eventsLog.events {
+                if events.isEmpty {
+                    Spacer()
+                    if eventsLog.searchText.isEmpty {
+                        Text("No events logged.")
+                    } else {
+                        Text("No events found matching search.")
+                    }
+                    Spacer()
+                } else {
+                    eventsView(events)
+                        .searchable(text: $eventsLog.searchText)
+                }
+            } else {
+                Spacer()
+
+                ProgressView("Loading...")
+
+                Spacer()
+            }
+        }
+        .navigationTitle("Events Log")
+    }
+
+    @ViewBuilder
+    private func eventsView(_ events: [Event]) -> some View {
         List {
             ForEach(events) { event in
                 Section(event.date.formatted()) {
@@ -43,24 +46,10 @@ struct EventsLogView: View {
                 }
             }
             .onDelete { indexSet in
-                let events = indexSet.map { self.events[$0] }
-                events.forEach { event in
-                    switch event {
-                    case .replacedLinks(let event):
-                        // This would only be triggered when deleting the section, which shouldn't
-                        // currently be possible because each link is displayed individually by a
-                        // `ForEach` that has its own `onDelete` modifier.
-                        guard let index = replacedLinks.firstIndex(where: { $0.id == event.id }) else { return }
-                        replacedLinks.remove(at: index)
-                    case .redirectedLink(let event):
-                        guard let index = redirectedLinks.firstIndex(where: { $0.id == event.id }) else { return }
-                        redirectedLinks.remove(at: index)
-                    }
-                }
+                let events = indexSet.map { events[$0] }
+                eventsLog.deleteEvents(events)
             }
         }
-        .searchable(text: $searchText)
-        .navigationTitle("Events Log")
     }
 
     @ViewBuilder
@@ -71,16 +60,98 @@ struct EventsLogView: View {
             Text("Replaced link to \(domain)")
         }
         .onDelete { indexSet in
-            guard let index = replacedLinks.firstIndex(where: { $0.id == event.id }) else { return }
-
             var event = event
             event.domains.remove(atOffsets: indexSet)
 
-            if event.domains.isEmpty {
-                replacedLinks.remove(at: index)
-            } else {
-                replacedLinks[index] = event
+            eventsLog.updateReplacedLinksEvent(event)
+        }
+    }
+}
+
+private final class EventsLog: ObservableObject {
+    @Published private(set) var events: [Event]?
+
+    @Published var searchText: String = ""
+
+    @Persisted(persister: .replacedLinks)
+    private var replacedLinks: [ReplacedLinksEvent]
+
+    @Persisted(persister: .redirectedLinks)
+    private var redirectedLinks: [RedirectLinkEvent]
+
+    private var cancellables: Set<Combine.AnyCancellable> = []
+
+    init() {
+        let workQueue = DispatchQueue(label: "Events Log")
+
+        $replacedLinks
+            .publisher
+            .receive(on: workQueue)
+            .combineLatest(
+                $redirectedLinks
+                    .publisher
+            )
+            .map { parameters -> [Event] in
+                let (replacedLinks, redirectedLinks) = parameters
+                let allEvents = replacedLinks.map { Event.replacedLinks($0) } + redirectedLinks.map { Event.redirectedLink($0) }
+                return allEvents.sorted(by: { $0.date > $1.date })
             }
+            .combineLatest(
+                $searchText
+                    .prepend("")
+                    .debounce(for: 0.3, scheduler: workQueue)
+            )
+            .map { parameters -> [Event] in
+                var events = parameters.0
+                let searchText = parameters.1
+
+                print("Filtering")
+
+                if !searchText.isEmpty {
+                    events = events.filter { event -> Bool in
+                        switch event {
+                        case .replacedLinks(let event):
+                            return event.domains.contains { $0.localizedCaseInsensitiveContains(searchText) }
+                        case .redirectedLink(let event):
+                            return event.domain.localizedCaseInsensitiveContains(searchText)
+                        }
+                    }
+                }
+
+                return events
+            }
+            .receive(on: RunLoop.main)
+            .sink { events in
+                withAnimation {
+                    self.events = events
+                }
+            }
+            .store(in: &cancellables)
+    }
+
+    func deleteEvents(_ events: [Event]) {
+        events.forEach { event in
+            switch event {
+            case .replacedLinks(let event):
+                // This would only be triggered when deleting the section, which shouldn't
+                // currently be possible because each link is displayed individually by a
+                // `ForEach` that has its own `onDelete` modifier.
+                guard let index = replacedLinks.firstIndex(where: { $0.id == event.id }) else { return }
+                replacedLinks.remove(at: index)
+            case .redirectedLink(let event):
+                guard let index = redirectedLinks.firstIndex(where: { $0.id == event.id }) else { return }
+                redirectedLinks.remove(at: index)
+            }
+        }
+    }
+
+    func updateReplacedLinksEvent(_ event: ReplacedLinksEvent) {
+        guard let index = replacedLinks.firstIndex(where: { $0.id == event.id }) else { return }
+
+        if event.domains.isEmpty {
+            replacedLinks.remove(at: index)
+        } else {
+            replacedLinks[index] = event
         }
     }
 }
