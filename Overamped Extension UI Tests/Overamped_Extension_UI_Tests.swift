e3b2d9c2d6c1b4f29605f320d9395dc3d433a5e5
Joseph Duffy <commits@josephduffy.co.uk>
2025-01-29 19:15:59 +0000

    Use Install Checker in UI tests, align with Safari in iOS 18

--- Overamped Extension UI Tests/Overamped_Extension_UI_Tests.swift
+++ Overamped Extension UI Tests/Overamped_Extension_UI_Tests.swift
@@ -1,59 +1,61 @@
 import XCTest
 
+@MainActor
 final class Overamped_Extension_UI_Tests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
 
     func testEnablingRemovesAllAMPLogos() throws {
-        // Main app must've been launched for share extension to be available
+        // Main app must've been launched for web extension to be available.
         let app = XCUIApplication()
         app.launch()
 
         let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
         safari.activate()
 
-        func tapBottomBar() {
-            safari.coordinate(withNormalizedOffset: CGVector(dx: UIScreen.main.bounds.width / 2, dy: UIScreen.main.bounds.width - 5)).tap()
+        // Create a new tab.
+        let tabOverviewButton = safari.buttons["TabOverviewButton"]
+        let newTabButton = safari.toolbars.buttons["NewTabButton"]
+
+        if !newTabButton.exists, tabOverviewButton.exists {
+            tabOverviewButton.tap()
         }
 
-        // Used to be necessary in iOS 15 beta 2 but Safari now opens with the bottom bar expanded in beta 3
-//        tapBottomBar()
+        newTabButton.tap()
 
-        // Create a new tab
-        safari.buttons["TabOverviewButton"].tap()
-        safari.buttons["AddTabButton"].tap()
+        // Navigate to install checker.
+        let addressTextField = safari.textFields["Address"]
+        addressTextField.tap()
+        addressTextField.typeText("https://overamped.app/install-checker\n")
 
-        safari.textFields.firstMatch.typeText("iOS 15 beta news\n")
-
-        tapBottomBar()
-
-        // Ensure extension is initially disabled
-        safari.buttons["MoreOptionsButton"].tap()
-        safari.collectionViews.buttons["Extensions"].tap()
+        // Enable extension.
+        safari.buttons["PageFormatMenuButton"].tap()
+        safari.cells["ManageExtensions"].tap()
 
         let overampedExtensionSwitch = safari.switches["Overamped"]
 
-        if overampedExtensionSwitch.value as? String == "1" {
+        if overampedExtensionSwitch.value as? String == "0" {
             overampedExtensionSwitch.tap()
         }
 
         safari.buttons["Done"].tap()
-        safari.buttons["Reload"].tap()
+        safari.cells["Overamped"].tap()
 
-        XCTAssertTrue(safari.links.element(matching: NSPredicate(format: "label == \"AMP logo\"")).waitForExistence(timeout: 5))
+        if safari.alerts.count > 0 {
+            // Permission alert it showing.
+            safari.alerts.buttons["Always Allow…"].tap()
+            safari.alerts.buttons["Always Allow on Every Website"].tap()
+        }
 
-        // Enable extension
-        tapBottomBar()
-        safari.buttons["MoreOptionsButton"].tap()
-        safari.collectionViews.buttons["Extensions"].tap()
-        overampedExtensionSwitch.tap()
-        // Close Extensions sheet
+        if !safari.buttons["Done"].exists {
+            safari.cells["Overamped"].tap()
+        }
         safari.buttons["Done"].tap()
-        // Close partial sheet
-        safari.buttons["Close"].tap()
 
-        XCTAssertTrue(safari.links.element(matching: NSPredicate(format: "label == \"Images\"")).waitForExistence(timeout: 5))
-        XCTAssertFalse(safari.links.element(matching: NSPredicate(format: "label == \"AMP logo\"")).exists)
+        // Check that install checker detected the extension.
+        let extensionDetectedText = safari.staticTexts["✅ Overamped Safari Extension Detected"]
+
+        XCTAssertTrue(extensionDetectedText.waitForExistence(timeout: 5))
     }
 }
